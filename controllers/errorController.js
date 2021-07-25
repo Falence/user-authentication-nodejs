f6395618d796d7aaf177240d1fe3ae590c351e98
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-25 21:27:09 +0100

    implement basic error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,20 @@
-module.exports = (err, req, res, next) => {
 
+const sendDevError = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    })
+}
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+
+    if (process.env.NODE_ENV === 'development') {
+        sendDevError(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        // sendProdError()
+    }
 }
