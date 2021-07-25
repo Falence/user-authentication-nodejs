f7fd4a819698968b504d8effe305a60e6aa1b8b0
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-25 21:54:35 +0100

    Handle promise errors by directing them to the global error handler

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+exports.signup = (req, res, next) => {
+    
+}
+
+exports.login = (req, res, next) => {}
+
+exports.protect = (req, res, next) => {}
+
+exports.restrictTo = (req, res, next) => {}
+
+exports.forgotPassword = (req, res, next) => {}
+
+exports.resetPassword = (req, res, next) => {}
+
+exports.updatePassword = (req, res, next) => {}
