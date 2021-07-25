86343c51f2dfaf42bc29168cd684134443b33af5
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-25 21:42:42 +0100

    implement basic user model

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name!']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email address!'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password!'],
+        minlength: 8,
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password!'],
+        validate: {
+            validator: function(confirmPassword) {
+                return confirmPassword === this.password
+            },
+            message: 'Passwords are not the same!'
+        }
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
