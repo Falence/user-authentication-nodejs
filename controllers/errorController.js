75c80620e28864d89d9f9cb87ea46db7e34a133a
Falence Lemungoh <falencelemungoh@gmail.com>
2021-07-26 09:36:11 +0100

    Handle Errors for production environment: duplicate field error and validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,14 @@
+const AppError = require('./../utils/appError')
+
+const handleDuplicateFieldDB = err => {
+    return new AppError(`${err.keyValue.email} has already been taken. Use a different one!`, 400)
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message)
+    const message = `Invalid data input: ${errors.join(' ')}`
+    return new AppError(message, 400)
+}
 
 const sendDevError = (err, res) => {
     res.status(err.statusCode).json({
@@ -8,6 +19,22 @@ const sendDevError = (err, res) => {
     })
 }
 
+const sendProdError = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    } else {
+        console.log(`ERROR: ${err}`) 
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong'
+        })
+    }
+}
+
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
@@ -15,6 +42,8 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendDevError(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        // sendProdError()
+        if (err.code === 11000) err = handleDuplicateFieldDB(err)
+        if (err.name === 'ValidationError') err = handleValidationErrorDB(err)
+        sendProdError(err, res)
     }
 }
